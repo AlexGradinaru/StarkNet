7777d90aed3929cda33a9bfb8405496d7485dd62
AlexGradinaru <56272611+AlexGradinaru@users.noreply.github.com>
2022-03-25 11:50:46 +0200

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-sudo apt install curl
-
 curl -s https://raw.githubusercontent.com/cryptongithub/init/main/logo.sh | bash && sleep 2
+sudo apt install curl
+apt install git -y
+
+
 
 sudo apt update && sudo apt full-upgrade -y
 sudo apt install -y python3-pip
@@ -11,7 +13,8 @@ pip3 install fastecdsa
 sudo apt-get install -y pkg-config
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh 
+source $HOME/.cargo/env
 sudo apt install cargo
-rustup update stable
+
 
 git clone --branch v0.1.6-alpha https://github.com/eqlabs/pathfinder.git
