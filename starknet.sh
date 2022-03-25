00c45f38280ba07bd349aed923b8f3c46befeb34
AlexGradinaru <56272611+AlexGradinaru@users.noreply.github.com>
2022-03-25 11:53:26 +0200

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -6,15 +6,15 @@ apt install git -y
 
 
 sudo apt update && sudo apt full-upgrade -y
-sudo apt install -y python3-pip
-sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
-sudo apt-get install libgmp-dev
-pip3 install fastecdsa
-sudo apt-get install -y pkg-config
+sudo apt install -y python3-pip -y
+sudo apt install -y build-essential libssl-dev libffi-dev python3-dev -y
+sudo apt-get install libgmp-dev -y
+pip3 install fastecdsa -y
+sudo apt-get install -y pkg-config -y
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
 source $HOME/.cargo/env
-sudo apt install cargo
+sudo apt install cargo -y
 
 
 git clone --branch v0.1.6-alpha https://github.com/eqlabs/pathfinder.git
