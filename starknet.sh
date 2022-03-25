1fb95a928fe6fb5c5471968df52587c29d71a53d
AlexGradinaru <56272611+AlexGradinaru@users.noreply.github.com>
2022-03-25 14:31:32 +0200

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -15,14 +15,14 @@ sudo apt-get install libgmp-dev -y
 pip3 install fastecdsa -y
 sudo apt-get install -y pkg-config -y
 
-function installRust {
-	echo -e '\n\e[42mInstall Rust\e[0m\n' && sleep 1
-	# sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
-	curl https://getsubstrate.io -sSf | bash -s -- --fast 
-	source $HOME/.cargo/env
-}
 
-installRust
+echo -e '\n\e[42mInstall Rust\e[0m\n' && sleep 1
+# sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
+curl https://getsubstrate.io -sSf | bash -s -- --fast 
+source $HOME/.cargo/env
+
+
+
 
 git clone --branch v0.1.6-alpha https://github.com/eqlabs/pathfinder.git
 
@@ -31,11 +31,6 @@ git clone --branch v0.1.6-alpha https://github.com/eqlabs/pathfinder.git
 sudo apt install python3.8-venv
 
 python3 -m venv .venv
-
-
-PIP_REQUIRE_VIRTUALENV=true pip install --upgrade pip
-
-PIP_REQUIRE_VIRTUALENV=true pip install -r requirements-dev.txt
 
 
 
