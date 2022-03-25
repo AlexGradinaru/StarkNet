178d371ba22168a0dd2f2ce5d313ff3768b95599
AlexGradinaru <56272611+AlexGradinaru@users.noreply.github.com>
2022-03-25 13:25:08 +0200

    Update starknet6.sh

--- starknet6.sh
+++ starknet6.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+echo -e "\e[1m\e[32m2. Installing Rust... \e[0m" && sleep 1
 echo "=================================================="
 cd pathfinder/py
+echo -e "\e[1m\e[32m2. Installing Rust... \e[0m" && sleep 1
 echo "=================================================="
+echo -e "\e[1m\e[32m2. Installing Rust... \e[0m" && sleep 1
 source .venv/bin/activate
 echo "=================================================="
