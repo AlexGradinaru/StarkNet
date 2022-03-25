9081e546a3455f7174d2625df7ce7753b0d1fa63
AlexGradinaru <56272611+AlexGradinaru@users.noreply.github.com>
2022-03-25 13:05:39 +0200

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-curl -s https://raw.githubusercontent.com/cryptongithub/init/main/logo.sh | bash && sleep 2
+
 sudo apt install curl
+
+curl -s https://raw.githubusercontent.com/cryptongithub/init/main/logo.sh | bash && sleep 2
+
 apt install git -y
 
 
@@ -14,7 +17,13 @@ sudo apt-get install -y pkg-config -y
 
 sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
-sudo apt install cargo -y
+rustup update
+rustup update nightly
+rustup target add wasm32-unknown-unknown --toolchain nightly
+
 
 
 git clone --branch v0.1.6-alpha https://github.com/eqlabs/pathfinder.git
+
+
+
